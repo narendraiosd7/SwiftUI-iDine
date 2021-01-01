1e3c58c14d6a4ed078d147a52284702dd1c5519b
narendra.vadde <narendra.vadde@Pavans-Mac-mini.local>
2021-01-01 18:23:41 +0530

    bindingsAndForms

---
+++ SwiftUI-iDine/CheckoutView.swift
@@ -0,0 +1,66 @@
+//
+//  CheckoutView.swift
+//  SwiftUI-iDine
+//
+//  Created by narendra.vadde on 01/01/21.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [0, 10, 15, 20, 25]
+    
+    @State private var paymentType = 0
+    @State private var addLoyalityDetails = false
+    @State private var loyalityNumber = ""
+    @State private var tipAmount = 2
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0 ..< Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+                
+                Toggle(isOn: $addLoyalityDetails.animation()) {
+                    Text("Add iDine loyality card")
+                }
+                
+                if addLoyalityDetails {
+                    TextField("Enter your iDine ID", text: $loyalityNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(0 ..< Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("TOTAL: $100")) {
+                Button("Confirm Order") {
+                    
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    
+    static let order = Order()
+    
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
