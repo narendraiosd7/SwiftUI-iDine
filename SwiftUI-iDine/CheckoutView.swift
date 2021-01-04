28695d3e3535d4aa83a1385b56cfc3190f0f25dc
narendra.vadde <narendra.vadde@Pavans-Mac-mini.local>
2021-01-04 10:39:23 +0530

    interpolatedStrings

--- SwiftUI-iDine/CheckoutView.swift
+++ SwiftUI-iDine/CheckoutView.swift
@@ -19,6 +19,12 @@ struct CheckoutView: View {
     @State private var loyalityNumber = ""
     @State private var tipAmount = 2
     
+    var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -46,7 +52,7 @@ struct CheckoutView: View {
                 .pickerStyle(SegmentedPickerStyle())
             }
             
-            Section(header: Text("TOTAL: $100")) {
+            Section(header: Text("TOTAL: $\(totalPrice, specifier: "%.2f")")) {
                 Button("Confirm Order") {
                     
                 }
